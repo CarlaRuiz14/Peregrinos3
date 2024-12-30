d56b574540f45cd33df0c96162779d857eb56a4e
Usuario <Usuario@carlaptop.Home>
2024-12-30 13:18:34 +0100

    RegParada

--- src/main/java/com/luisdbb/tarea3AD2024base/controller/RegParadaController.java
+++ src/main/java/com/luisdbb/tarea3AD2024base/controller/RegParadaController.java
@@ -13,16 +13,24 @@ import com.luisdbb.tarea3AD2024base.services.UsuarioService;
 import com.luisdbb.tarea3AD2024base.view.FxmlView;
 
 import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 
 /**
  * @author Carla Ruiz
@@ -32,31 +40,37 @@ import javafx.scene.image.ImageView;
 public class RegParadaController implements Initializable {
 
 	@FXML
-	private Label lblLogin;
+	private Hyperlink hpInfo;
+
+	@FXML
+	private TextField txtUsuario;
 	
 	@FXML
-	private ImageView imgUsuario;
-
-	@FXML
-	private TextField txtUsuario;	
-
-	@FXML
-	private ImageView imgContraseña;
-
+	private TextField txtEmail;
+	
 	@FXML
 	private TextField txtContraseña;
+	
+	@FXML
+	private TextField txtConfirmacion;
 
 	@FXML
-	private Hyperlink hpContraseña;
+	private TextField txtNombreP;
+	
+	@FXML
+	private TextField txtRegionP;
+	
+	@FXML
+	private Button btnLimpiar;
 
 	@FXML
-	private Hyperlink hpRegistro;
-
-	@FXML
-	private Button btnLogin;
+	private Button btnRegistrar;
 
 	@FXML
 	private Button btnVolver;
+
+	@FXML
+	private Label lblFeed;
 
 	@FXML
 	private Button btnSalir;
@@ -69,24 +83,33 @@ public class RegParadaController implements Initializable {
 	@Autowired
 	private StageManager stageManager;
 
-	public String getContraseña() {
-		return txtContraseña.getText();
-	}
-
-	public String getUsuario() {
-		return txtUsuario.getText();
-	}
-
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		// configuracion imagen boton Login
-		String rutaLogin = resources.getString("btnLogin.icon");
-		Image imgLogin = new Image(getClass().getResourceAsStream(rutaLogin));
-		ImageView viewLogin = new ImageView(imgLogin);
-		viewLogin.setFitWidth(60);
-		viewLogin.setFitHeight(30);
-		btnLogin.setGraphic(viewLogin);
+		// configuracion info
+		String rutaInfo = resources.getString("info.icon");
+		Image imagen = new Image(getClass().getResourceAsStream(rutaInfo));
+		ImageView imageView = new ImageView(imagen);
+		imageView.setFitWidth(30);
+		imageView.setFitHeight(30);
+		imageView.setPreserveRatio(true);
+		hpInfo.setGraphic(imageView);
+
+		// configuracion imagen boton Limpiar
+		String rutaLimp = resources.getString("btnLimpiar.icon");
+		Image imgLimp = new Image(getClass().getResourceAsStream(rutaLimp));
+		ImageView viewLimp = new ImageView(imgLimp);
+		viewLimp.setFitWidth(30);
+		viewLimp.setFitHeight(30);
+		btnLimpiar.setGraphic(viewLimp);
+
+		// configuracion imagen boton Registrar
+		String rutaReg = resources.getString("btnRegistrar.icon");
+		Image imgReg = new Image(getClass().getResourceAsStream(rutaReg));
+		ImageView viewReg = new ImageView(imgReg);
+		viewReg.setFitWidth(60);
+		viewReg.setFitHeight(30);
+		btnRegistrar.setGraphic(viewReg);
 
 		// configuracion imagen boton Volver
 		String rutaVolver = resources.getString("btnVolver.icon");
@@ -104,37 +127,78 @@ public class RegParadaController implements Initializable {
 		viewSalir.setFitHeight(20);
 		btnSalir.setGraphic(viewSalir);
 
-		// configuracion imagenes
-		String rutaUsu = resources.getString("usuario.icon");
-		imgUsuario.setImage(new Image(getClass().getResourceAsStream(rutaUsu)));
+		// mnenomicos
+		hpInfo.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			if (event.isAltDown() && event.getCode() == KeyCode.I) {
+				hpInfo.fire();
+				event.consume();
+			}
+		});
 
-		String rutaCon = resources.getString("contraseña.icon");
-		imgContraseña.setImage(new Image(getClass().getResourceAsStream(rutaCon)));
+		btnLimpiar.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			if (event.isAltDown() && event.getCode() == KeyCode.L) {
+				btnLimpiar.fire();
+				event.consume();
+			}
+		});
 
-		
+		btnRegistrar.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			if (event.isAltDown() && event.getCode() == KeyCode.R) {
+				btnRegistrar.fire();
+				event.consume();
+			}
+		});
+
+		btnVolver.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			if (event.isAltDown() && event.getCode() == KeyCode.V) {
+				btnVolver.fire();
+				event.consume();
+			}
+		});
+
+		btnSalir.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+			if (event.isAltDown() && event.getCode() == KeyCode.S) {
+				btnSalir.fire();
+				event.consume();
+			}
+		});
+
+		// tooltips
+		hpInfo.setTooltip(new Tooltip("Info (Alt+I)"));
+		btnLimpiar.setTooltip(new Tooltip("Limpiar (Alt+L)"));
+		btnRegistrar.setTooltip(new Tooltip("Registrar (Alt+R)"));
+		btnVolver.setTooltip(new Tooltip("Volver (Alt+V)"));
+		btnSalir.setTooltip(new Tooltip("Salir (Alt+S)"));
+
 	}
 
 	// handler botones
 
 	@FXML
-	private void handlerLogin(ActionEvent event) throws IOException {
-		if (userService.authenticate(getUsuario(), getContraseña())) {
+	private void handlerRegistrar(ActionEvent event) throws IOException {
 
-			stageManager.switchScene(FxmlView.USER);
+	}
 
-		} else {
-			lblLogin.setText("Login Failed.");
-		}
+	@FXML
+	private void handlerLimpiar(ActionEvent event) throws IOException {
+
 	}
 
 	@FXML
 	private void handlerVolver(ActionEvent event) throws IOException {
-		stageManager.switchScene(FxmlView.MAIN);
+		stageManager.switchScene(FxmlView.LOGIN);
 	}
 
 	@FXML
 	private void handlerSalir(ActionEvent event) throws IOException {
 		Platform.exit();
+	}
+
+	// handler info
+
+	@FXML
+	private void handlerInfo(ActionEvent event) throws IOException {
+
 	}
 
 }
